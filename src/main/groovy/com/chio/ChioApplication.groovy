0226553639b9d866f18d75493de1ed90aefb035a
Matt Chiogioji <mattchiogioji@gmail.com>
2017-04-18 21:05:27 -0400

    Initial commit

---
+++ src/main/groovy/com/chio/ChioApplication.groovy
@@ -0,0 +1,16 @@
+package com.chio
+
+import groovy.util.logging.Slf4j
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+@Slf4j
+public class ChioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChioApplication.class, args);
+	}
+
+
+}
---
+++ src/main/groovy/com/chio/fixtures/QueryFlightApi.groovy
@@ -0,0 +1,30 @@
+package com.chio.fixtures
+
+class QueryFlightApi {
+
+    String testDescription
+    String origin
+    String destination
+    String date;
+    int returnCode
+    int resultSize
+    String resultDestination
+
+
+    void execute() {
+
+    }
+
+    int returnCode() {
+        return returnCode
+    }
+
+    int resultSize() {
+        return resultSize
+    }
+
+    String resultDestination() {
+        return resultDestination
+    }
+
+}
